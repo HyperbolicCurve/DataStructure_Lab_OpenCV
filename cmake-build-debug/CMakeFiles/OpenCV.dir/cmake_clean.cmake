20dcd5aecd9deb877a484ca67f21b1835bf2fd25
Xianjin_Wu <1278117059@qq.com>
2023-11-11 16:25:47 +0800

    Successful edition with Powell algorithm, tips from my classmates!

---
+++ cmake-build-debug/CMakeFiles/OpenCV.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCV.dir/4_Color.cpp.obj"
+  "CMakeFiles/OpenCV.dir/4_Color.cpp.obj.d"
+  "CMakeFiles/OpenCV.dir/main.cpp.obj"
+  "CMakeFiles/OpenCV.dir/main.cpp.obj.d"
+  "CMakeFiles/OpenCV.dir/poisson.cpp.obj"
+  "CMakeFiles/OpenCV.dir/poisson.cpp.obj.d"
+  "OpenCV.exe"
+  "OpenCV.exe.manifest"
+  "OpenCV.pdb"
+  "libOpenCV.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
